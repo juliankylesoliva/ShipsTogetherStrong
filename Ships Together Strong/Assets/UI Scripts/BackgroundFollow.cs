388d1f8b61824138c73e1016294b0e069c26713f
jsoliv2 <68832077+juliankylesoliva@users.noreply.github.com>
2021-07-09 20:56:09 -0500

    Camera zoomed out

--- Ships Together Strong/Assets/UI Scripts/BackgroundFollow.cs
+++ Ships Together Strong/Assets/UI Scripts/BackgroundFollow.cs
@@ -5,26 +5,27 @@ using UnityEngine;
 public class BackgroundFollow : MonoBehaviour
 {
     public Transform playerPosition;
+    public float updateRadius = 15.0f;
 
     void Update()
     {
         Vector3 distanceVec = playerPosition.position - this.transform.position;
 
-        if (distanceVec.x > 25.0f)
+        if (distanceVec.x > updateRadius)
         {
             this.transform.position += (Vector3.right * 25.0f);
         }
-        else if (distanceVec.x < -25.0f)
+        else if (distanceVec.x < -updateRadius)
         {
             this.transform.position += (Vector3.left * 25.0f);
         }
         else { }
 
-        if (distanceVec.y > 25.0f)
+        if (distanceVec.y > updateRadius)
         {
             this.transform.position += (Vector3.up * 25.0f);
         }
-        else if (distanceVec.y < -25.0f)
+        else if (distanceVec.y < -updateRadius)
         {
             this.transform.position += (Vector3.down * 25.0f);
         }
